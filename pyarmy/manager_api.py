4160609c7f52533735bce15773c6f6c32e36d029
jackjtaylor <jackjta@outlook.com.au>
2024-06-01 04:24:19 +1000

    feat(manager_api.py): add tinydb

--- pyarmy/manager_api.py
+++ pyarmy/manager_api.py
@@ -1,6 +1,8 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from tinydb import TinyDB
 
+db = TinyDB("db.json")
 app = FastAPI()
 
 
@@ -14,5 +16,24 @@ async def get_role() -> str:
     return "Manager"
 
 
+@app.post("/send/{cmd}")
+async def send_cmd(cmd: str):
+    table = db.table("workers")
+    workers = table.all()
+    for w in workers:
+        print(w.keys())
+        pass
+
+
+@app.get("/connect")
+async def get_new_worker(request: Request):
+    if not request.client:
+        return
+
+    workers = db.table("workers")
+    configuration = {"status": "Online", "uptime": 0}
+    workers.insert({request.client.host: configuration})
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9393)
+    uvicorn.run(app, host="0.0.0.0", port=9255)
